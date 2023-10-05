cd238754fef8c257bb19ea74d7078dad34f5babf
Phan Huy Hiệu <114344641+phanhuyhieu2004@users.noreply.github.com>
2023-10-05 20:06:35 +0700

    Update Prime.java

--- src/main/java/Buoi7/Prime.java
+++ src/main/java/Buoi7/Prime.java
@@ -32,30 +32,3 @@ public class Prime {
 
 }
 
-//    Nếu số number được truyền vào là 7, đoạn mã trên sẽ chạy như sau:
-//
-//        Biến i được khởi tạo với giá trị ban đầu là 2.
-//        Kiểm tra điều kiện i < number. Vì 2 < 7, điều kiện đúng.
-//        Kiểm tra điều kiện number % i == 0. Vì 7 không chia hết cho 2 (7 % 2 = 1), điều kiện không đúng.
-//        Tăng giá trị của i lên 1, i trở thành 3.
-//        Quay lại bước 2 để kiểm tra tiếp.
-//        Kiểm tra điều kiện i < number. Vì 3 < 7, điều kiện đúng.
-//        Kiểm tra điều kiện number % i == 0. Vì 7 không chia hết cho 3 (7 % 3 = 1), điều kiện không đúng.
-//        Tăng giá trị của i lên 1, i trở thành 4.
-//        Quay lại bước 2 để kiểm tra tiếp.
-//        Kiểm tra điều kiện i < number. Vì 4 < 7, điều kiện đúng.
-//        Kiểm tra điều kiện number % i == 0. Vì 7 không chia hết cho 4 (7 % 4 = 3), điều kiện không đúng.
-//        Tăng giá trị của i lên 1, i trở thành 5.
-//        Quay lại bước 2 để kiểm tra tiếp.
-//        Kiểm tra điều kiện i < number. Vì 5 < 7, điều kiện đúng.
-//        Kiểm tra điều kiện number % i == 0. Vì 7 không chia hết cho 5 (7 % 5 = 2), điều kiện không đúng.
-//        Tăng giá trị của i lên 1, i trở thành 6.
-//        Quay lại bước 2 để kiểm tra tiếp.
-//        Kiểm tra điều kiện i < number. Vì 6 < 7, điều kiện đúng.
-//        Kiểm tra điều kiện number % i == 0. Vì 7 không chia hết cho 6 (7 % 6 = 1), điều kiện không đúng.
-//        Tăng giá trị của i lên 1, i trở thành 7.
-//        Quay lại bước 2 để kiểm tra tiếp.
-//        Kiểm tra điều kiện i < number. Vì 7 không nhỏ hơn 7, điều kiện không đúng.
-//        Kết thúc vòng lặp for.
-//        Vì không tìm thấy số i từ 2 đến 6 mà chia hết cho 7, nên số 7 được coi là số nguyên tố.
-//        Phương thức trả về true.
